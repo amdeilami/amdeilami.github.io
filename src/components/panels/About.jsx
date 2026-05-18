027554eb82133fc553eae18a81e56ccc0c990668
AmirMohammad Deilami <a.m.deilami@gmail.com>
2026-05-17 21:40:02 -0700

    updated the about page

--- src/components/panels/About.jsx
+++ src/components/panels/About.jsx
@@ -8,31 +8,28 @@ export default function About({ active, onClose }) {
         <img src="/images/Amir_in_Nature.jpg" alt="" />
       </span>
       <p style={{ textAlign: 'justify' }}>
-        Currently, I hold a Master&apos;s degree in computer science. I love learning.
-        <br />
-        In addition to software engineering, I like to watch brain-engaging movies and chess,
-        hiking and cooking.
+        I hold a Master&apos;s degree in Computer Science and have a genuine passion for
+        continuous learning. Outside of software engineering, I enjoy intellectually
+        stimulating films, chess, hiking, and cooking.
       </p>
-      <p>
-        <img
-          src="/images/sfu-logo.png"
-          alt="Simon Fraser University logo"
-          width="10%"
-          style={{ float: 'left', marginRight: '1em' }}
-        />
-        <b>MSc, Computer Science</b> <br /> (2022&ndash;2023)
-        <br /> Simon Fraser University, Burnaby, British Columbia, Canada
-      </p>
-      <p>
-        <img
-          src="/images/SBU_Logo.png"
-          alt="Shahid Beheshti University logo"
-          width="10%"
-          style={{ float: 'left', marginRight: '1em' }}
-        />
-        <b>B.Sc., Computer Engineering</b> <br /> (2014&ndash;2019)
-        <br /> Shahid Beheshti University, Tehran, Tehran province, Iran
-      </p>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '1em', marginBottom: '1.5em' }}>
+        <img src="/images/sfu-logo.png" alt="Simon Fraser University logo"
+          style={{ width: '60px', flexShrink: 0 }} />
+        <div>
+          <b>MSc, Computer Science</b><br />
+          (2022&ndash;2023)<br />
+          Simon Fraser University, Burnaby, British Columbia, Canada
+        </div>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '1em', marginBottom: '1.5em' }}>
+        <img src="/images/SBU_Logo.png" alt="Shahid Beheshti University logo"
+          style={{ width: '60px', flexShrink: 0 }} />
+        <div>
+          <b>B.Sc., Computer Engineering</b><br />
+          (2014&ndash;2019)<br />
+          Shahid Beheshti University, Tehran, Tehran province, Iran
+        </div>
+      </div>
     </Panel>
   )
 }
